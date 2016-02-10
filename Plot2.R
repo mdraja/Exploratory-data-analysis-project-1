edc3125fc23a526a77727621d0490a083b508ff4
mdraja <mdraja137@gmail.com>
2016-02-10 20:05:35 +0530

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -55,4 +55,4 @@ png(filename='plots/plot2.png',width=480,height=480,units='px')
 plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power (kilowatts)', xlab='', type='l')
 
 # close device
-# x<-dev.off()
+x<-dev.off()
